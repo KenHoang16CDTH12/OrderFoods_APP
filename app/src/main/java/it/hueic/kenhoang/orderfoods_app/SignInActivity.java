65a25eb0efd819c1acb2661b5628bd165e0cf5a0
Duong Minh Hoang <angelkittybihoang@gmail.com>
2018-01-26 12:30:03 +0700

    SignIn SignUp

---
+++ app/src/main/java/it/hueic/kenhoang/orderfoods_app/SignInActivity.java
@@ -0,0 +1,81 @@
+package it.hueic.kenhoang.orderfoods_app;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import it.hueic.kenhoang.orderfoods_app.model.User;
+
+public class SignInActivity extends AppCompatActivity {
+    private MaterialEditText edPhone, edPass;
+    private ProgressDialog mProgressbar;
+    private Button btnSignIn;
+    private DatabaseReference mDataUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        //InitViews
+        initViews();
+        //InitFireBase
+        mDataUser = FirebaseDatabase.getInstance().getReference("User");
+        //InitEvents
+        initEvents();
+    }
+
+    private void initEvents() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgressbar.setMessage("Logging ...");
+                mProgressbar.show();
+                mDataUser.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Check if user not exist in database
+                        if (dataSnapshot.child(edPhone.getText().toString()).exists()) {
+                            //Get User information
+                            mProgressbar.dismiss();
+                            User user = dataSnapshot
+                                    .child(edPhone.getText().toString())
+                                    .getValue(User.class);
+                            if (user.getPassword().equals(edPass.getText().toString())) {
+                                Toast.makeText(SignInActivity.this, "Sign In success ...", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignInActivity.this, "Wrong password ...", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            mProgressbar.dismiss();
+                            Toast.makeText(SignInActivity.this, "User not exist in database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void initViews() {
+        edPhone         = findViewById(R.id.edPhone);
+        edPass          = findViewById(R.id.edPass);
+        btnSignIn       = findViewById(R.id.btnSignIn);
+        mProgressbar    = new ProgressDialog(this);
+    }
+}
---
+++ app/src/main/java/it/hueic/kenhoang/orderfoods_app/SignUpActivity.java
@@ -0,0 +1,76 @@
+package it.hueic.kenhoang.orderfoods_app;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import it.hueic.kenhoang.orderfoods_app.model.User;
+
+public class SignUpActivity extends AppCompatActivity {
+    private MaterialEditText edPhone, edPass, edName;
+    private ProgressDialog mProgressbar;
+    private Button btnSignUp;
+    private DatabaseReference mDataUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        //InitViews
+        initViews();
+        //InitFireBase
+        mDataUser = FirebaseDatabase.getInstance().getReference("User");
+        //InitEvents
+        initEvents();
+    }
+
+    private void initEvents() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgressbar.setMessage("Please waiting ....");
+                mProgressbar.show();
+                mDataUser.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(edPhone.getText().toString()).exists()) {
+                            mProgressbar.dismiss();
+                            Toast.makeText(SignUpActivity.this, "Phone number already register", Toast.LENGTH_SHORT).show();
+                        } else {
+                            mProgressbar.dismiss();
+                            User user = new User(edName.getText().toString(), edPass.getText().toString());
+                            mDataUser.child(edPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUpActivity.this, "Sign up successfully!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void initViews() {
+        edName          = findViewById(R.id.edName);
+        edPhone         = findViewById(R.id.edPhone);
+        edPass          = findViewById(R.id.edPass);
+        btnSignUp       = findViewById(R.id.btnSignUp);
+        mProgressbar    = new ProgressDialog(this);
+    }
+}
